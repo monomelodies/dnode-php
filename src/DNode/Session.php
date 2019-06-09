47e83e10546432c1b274dd6b3e26bc7edd00f6a7
Marijn Ophorst <marijn@monomelodies.nl>
2019-06-09 13:15:59 +0200

    modernize

--- src/DNode/Session.php
+++ src/DNode/Session.php
@@ -10,13 +10,13 @@ class Session extends EventEmitter
     public $id = '';
 
     // Wrapped local callbacks, by callback ID
-    private $callbacks = array();
+    private $callbacks = [];
 
     // Latest callback ID used
     private $cbId = 0;
 
     // Remote methods that were wrapped, by callback ID
-    private $wrapped = array();
+    private $wrapped = [];
 
     // Wrapped object
     private $wrapper;
@@ -27,7 +27,7 @@ class Session extends EventEmitter
     // Whether the session is ready for operation
     public $ready = false;
 
-    public function __construct($id, $wrapper)
+    public function __construct(string $id, object $wrapper)
     {
         $this->id = $id;
         $this->wrapper = $wrapper;
@@ -35,24 +35,24 @@ class Session extends EventEmitter
         $this->wrapper->remote =& $this->remote;
     }
 
-    public function start()
+    public function start() : void
     {
         // Send our methods to the other party
-        $this->request('methods', array($this->wrapper));
+        $this->request('methods', [$this->wrapper]);
     }
 
-    public function end()
+    public function end() : void
     {
         $this->emit('end');
         $this->removeAllListeners();
 
-        $this->callbacks = array();
-        $this->wrapped = array();
+        $this->callbacks = [];
+        $this->wrapped = [];
         $this->remote = null;
         $this->wrapper = null;
     }
 
-    public function request($method, $args)
+    public function request(string $method, array $args) : void
     {
         // Wrap callbacks in arguments
         $scrub = $this->scrub($args);
@@ -64,10 +64,10 @@ class Session extends EventEmitter
             'links' => $scrub['links']
         );
 
-        $this->emit('request', array($request));
+        $this->emit('request', [$request]);
     }
 
-    public function parse($line)
+    public function parse($line) : void
     {
         // TODO: Error handling for JSON parsing
         $msg = json_decode($line);
@@ -75,7 +75,7 @@ class Session extends EventEmitter
         $this->handle($msg);
     }
 
-    public function handle($req)
+    public function handle($req) : void
     {
         $session = $this;
 
@@ -84,24 +84,27 @@ class Session extends EventEmitter
 
         if ($req->method === 'methods') {
             // Got a methods list from the remote
-            return $this->handleMethods($args[0]);
+            $this->handleMethods($args[0]);
+            return;
         }
         if ($req->method === 'error') {
             // Got an error from the remote
-            return $this->emit('remoteError', array($args[0]));
+            $this->emit('remoteError', [$args[0]]);
+            return;
         }
         if (is_string($req->method)) {
-            if (is_callable(array($this, $req->method))) {
-                return call_user_func_array(array($this, $req->method), $args);
+            if (is_callable([$this, $req->method])) {
+                call_user_func_array([$this, $req->method], $args);
+                return;
             }
-            return $this->emit('error', array("Request for non-enumerable method: {$req->method}"));
+            return $this->emit('error', ["Request for non-enumerable method: {$req->method}"]);
         }
         if (is_numeric($req->method)) {
             call_user_func_array($this->callbacks[$req->method], $args);
         }
     }
 
-    private function handleMethods($methods)
+    private function handleMethods($methods) : void
     {
         if (!is_object($methods)) {
             $methods = new \StdClass();
@@ -111,22 +114,22 @@ class Session extends EventEmitter
             $this->remote->setMethod($key, $value);
         }
 
-        $this->emit('remote', array($this->remote));
+        $this->emit('remote', [$this->remote]);
         $this->ready = true;
         $this->emit('ready');
     }
 
-    private function scrub($obj)
+    private function scrub(object $obj) : array
     {
-        $paths = array();
-        $links = array();
+        $paths = [];
+        $links = [];
 
         // TODO: Deep traversal
         foreach ($obj as $id => $node) {
             if (is_object($node)) {
                 if ($node instanceof \Closure) {
                     $this->callbacks[$this->cbId] = $node;
-                    $paths[$this->cbId] = array($id);
+                    $paths[$this->cbId] = [$id];
                     $this->cbId++;
                     $obj[$id] = '[Function]';
                     continue;
@@ -142,27 +145,28 @@ class Session extends EventEmitter
                     $methodName = $method->getName();
 
                     $this->callbacks[$this->cbId] = function() use ($methodName, $node) {
-                        call_user_func_array(array($node, $methodName), func_get_args());
+                        call_user_func_array([$node, $methodName], func_get_args());
                     };
-                    $paths[$this->cbId] = array($id, $methodName);
+                    $paths[$this->cbId] = [$id, $methodName];
                     $this->cbId++;
                     $node->$methodName = '[Function]';
                 }
             }
         }
 
-        return array(
+        return [
             'arguments' => $obj,
             'callbacks' => $paths,
-            'links' => $links
-        );
+            'links' => $links.
+        ];
     }
 
     /**
      * Replace callbacks. The supplied function should take a callback
      * id and return a callback of its own.
      */
-    private function unscrub($msg) {
+    private function unscrub($msg) : array
+    {
         $args = $msg->arguments;
         $session = $this;
         foreach ($msg->callbacks as $id => $path) {
@@ -184,3 +188,4 @@ class Session extends EventEmitter
         return $args;
     }
 }
+
