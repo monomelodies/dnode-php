75a7c4b9c20c0de462e47c475c21250d27461f19
Marijn Ophorst <marijn@monomelodies.nl>
2019-06-10 12:54:37 +0200

    move to toast, v1

---
+++ tests/toast/functional.php
@@ -0,0 +1,33 @@
+<?php
+
+use React\EventLoop\StreamSelectLoop;
+use Gentry\Gentry\Wrapper;
+
+return function () : Generator {
+    /**
+     * @covers DNode\DNode::__construct
+     * @covers DNode\DNode::connect
+     * @covers DNode\DNode::listen
+     * @test
+     */
+    yield function () {
+        $captured = null;
+
+        $loop = new StreamSelectLoop;
+        $server = Wrapper::createObject(Monomelodies\DNode\DNode::class, $loop, new Monomelodies\DNode\Transformer);
+        $socket = $server->listen('tcp://127.0.0.1:5004');
+
+        $client = Wrapper::createObject(Monomelodies\DNode\DNode::class, $loop);
+        $client->connect('tcp://127.0.0.1:5004', function ($remote, $conn) use (&$captured, $socket) {
+            $remote->transform('fou', function ($transformed) use ($conn, &$captured, $socket) {
+                $captured = $transformed;
+                $conn->end();
+                $socket->shutdown();
+            });
+        });
+        $loop->run();
+
+        assert('FOO' === $captured);
+    };
+};
+
