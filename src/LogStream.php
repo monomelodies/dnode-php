8b53a0bbfaf6922ff8919ab5f7b83ed02229eb23
Marijn Ophorst <marijn@monomelodies.nl>
2019-06-10 07:50:53 +0200

    renamed

---
+++ src/LogStream.php
@@ -0,0 +1,23 @@
+<?php
+namespace Monomelodies\DNode;
+
+use React\Stream\ThroughStream;
+
+class LogStream extends ThroughStream
+{
+    private $prefix;
+
+    public function __construct($prefix)
+    {
+        parent::__construct();
+
+        $this->prefix = $prefix;
+    }
+
+    public function filter($data)
+    {
+        echo $this->prefix.': '.$data;
+
+        return $data;
+    }
+}
