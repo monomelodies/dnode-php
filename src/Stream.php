359e7fc45577803b39577f3674415ce541eb824c
Marijn Ophorst <marijn@monomelodies.nl>
2019-06-10 07:51:11 +0200

    renamed

---
+++ src/Stream.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Monomelodies\DNode;
+
+use React\Stream\{ WritableResourceStream, CompositeStream };
+use React\Socket\ConnectionInterface;
+
+class Stream
+{
+    private $dnode;
+
+    public function __construct(DNode $dnode, ConnectionInterface $conn, Session $client, callable $onReady = null)
+    {
+        $this->dnode = $dnode;
+
+        foreach ($this->dnode->stack as $middleware) {
+            call_user_func($middleware, [$client->instance, $client->remote, $client]);
+        }
+
+        $input = new InputStream($conn->stream, $this->dnode->getLoop());
+        $input->on('data', function ($data) {
+            var_dump($data);
+        });
+        $output = new WritableResourceStream($conn->stream, $this->dnode->getLoop());
+        $client->on('request', function (array $request) use ($output) : void {
+            $output->emit('data', [json_encode($request)."\n"]);
+        });
+
+        $this->stream = new CompositeStream($input, $output);
+
+        if ($onReady) {
+            $client->on('connection', function () use ($client, $onReady) {
+                echo 'aight';
+                call_user_func($onReady, $client->remote, $client);
+            });
+        }
+    }
+
+    public function __call($method, array $args)
+    {
+        return call_user_func([$this->stream, $method], ...$args);
+    }
+}
