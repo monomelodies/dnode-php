6ca25cd1b66057a82adb2ad7a3ae0472fee12432
Marijn Ophorst <marijn@monomelodies.nl>
2019-06-13 08:02:20 +0200

    start implementing some stuff from the original package

--- src/Remote.php
+++ src/Remote.php
@@ -13,11 +13,28 @@ class Remote
     private $connection;
     /** @var array */
     private $methods = [];
+    /** @var int */
+    private $callbackId = 0;
 
     public function __construct(Protocol $protocol, ConnectionInterface $connection)
     {
         $this->protocol = $protocol;
         $this->connection = $connection;
+        echo 'requesting methods';
+        $this->request('methods', [$this->protocol]);
+    }
+
+    public function request($method, $args)
+    {
+        // Wrap callbacks in arguments
+        $scrub = $this->scrub($args);
+        $request = [
+            'method' => $method,
+            'arguments' => $scrub['arguments'],
+            'callbacks' => $scrub['callbacks'],
+            'links' => $scrub['links']
+        ];
+        $this->connection->write(json_encode(['event' => 'request', 'data' => [$request]]));
     }
 
     public function getMethods() : array
@@ -41,6 +58,43 @@ class Remote
             throw new BadMethodCallException("Method {$method} not available");
         }
         call_user_func($this->methods[$method], ...$args);
+    }
+
+    private function scrub($obj) : array
+    {
+        $paths = [];
+        $links = [];
+        // TODO: Deep traversal
+        foreach ($obj as $id => $node) {
+            if (is_object($node)) {
+                if ($node instanceof \Closure) {
+                    $this->callbacks[$this->callbackId] = $node;
+                    $paths[$this->callbackId] = [$id];
+                    $this->callbackId++;
+                    $obj[$id] = '[Function]';
+                    continue;
+                }
+                $reflector = new \ReflectionClass($node);
+                $methods = $reflector->getMethods();
+                foreach ($methods as $method) {
+                    if (!$method->isPublic() || $method->isConstructor() || $method->isDestructor()) {
+                        continue;
+                    }
+                    $methodName = $method->getName();
+                    $this->callbacks[$this->callbackId] = function() use ($methodName, $node) {
+                        call_user_func_array([$node, $methodName], func_get_args());
+                    };
+                    $paths[$this->callbackId] = [$id, $methodName];
+                    $this->callbackId++;
+                    $node->$methodName = '[Function]';
+                }
+            }
+        }
+        return [
+            'arguments' => $obj,
+            'callbacks' => $paths,
+            'links' => $links
+        ];
     }
 }
 
